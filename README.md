2297618c42b7aea553c396cb021480a44150740e
wbryon <ward.bryon@sunnyset.com>
2024-01-18 18:35:47 +0100

    Small fixes to get the sprint order correct.

--- README.md
+++ README.md
@@ -17,6 +17,17 @@ jira.filter=project = ... order by created DESC
 jira.key.prefix=PRJ
 jira.filter.pages=1,1001,2001,3001
 download.location=
+export.columns=Key,Issue Type,Summary,EverESSt Domain,Epic Link Key,Story Points,Status,Sprint,EverESSt Team,Fix versions
+export.columns.treatment=RAW,RAW,RAW,RAW,RAW,RAW,RAW,LAST_ENTRY_ALPHABETICAL_SORT,RAW,ONE_ENTRY_VALIDATION
 
 Beware of jira.filter.pages, because of a Jira limitation, in order to do paging, the PRJ-xxx must actually exist in the project (not been deleted).
 That's why it must be an extensive list.
+
+The export columns are the columns that will be exported in the CSV file.
+The treatment is the way the column will be treated,
+either RAW (no treatment), 
+FIRST_ENTRY (only the first entry will be kept), 
+ONE_ENTRY_VALIDATION (all entries will be kept but an error will be logged)
+LAST_ENTRY_ALPHABETICAL_SORT (after sorting alphabetically, the last entry will be kept, this is mainly useful for the Sprint column)
+For this it is important that the next chronological sprint is alphabetically behind the previous one.
+    
--- src/main/java/com/cegeka/everesst/jiraexport/export/ExportColumnsTreatment.java
+++ src/main/java/com/cegeka/everesst/jiraexport/export/ExportColumnsTreatment.java
@@ -3,20 +3,37 @@ package com.cegeka.everesst.jiraexport.export;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public enum ExportColumnsTreatment {
 
         RAW {
             @Override
             public String treat(String value) {
-                return value;
+                return value.trim();
             }
         },
         LAST_ENTRY {
             @Override
             public String treat(String value) {
-                return value.split(",")[value.split(",").length - 1];
+                return value.split(",")[value.split(",").length - 1].trim();
             }
         },
+
+        LAST_ENTRY_ALPHABETICAL_SORT{
+            @Override
+            public String treat(String value) {
+                return Arrays.stream(value.split(",")).map(String::trim).sorted(Comparator.reverseOrder()).toList().get(0);
+            }
+        },
+
+    FIRST_ENTRY {
+        @Override
+        public String treat(String value) {
+            return value.split(",")[0].trim();
+        }
+    },
         ONE_ENTRY_VALIDATION {
             private static final Logger logger = LoggerFactory.getLogger(ExportColumnsTreatment.class);
             @Override
@@ -24,7 +41,7 @@ public enum ExportColumnsTreatment {
                 if(value.split(",").length > 1){
                     logger.error("More than one entry found, while only one was expected {}", value);
                 }
-                return value;
+                return value.trim();
             }
         };
 
