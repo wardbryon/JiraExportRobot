238dda75d26cc7af86f8d41b95eb0ffc7ce7e7ba
Ward Bryon <ward.bryon@sunnyset.com>
2025-09-13 22:34:50 +0200

    Everything working with the updated API

--- src/main/java/com/sunnyset/jira/jiraexport/rest/SearchForIssuesUsingJQL.java
+++ src/main/java/com/sunnyset/jira/jiraexport/rest/SearchForIssuesUsingJQL.java
@@ -1,7 +1,10 @@
 package com.sunnyset.jira.jiraexport.rest;
 
+import com.atlassian.jira.rest.client.api.GetCreateIssueMetadataOptions;
+import com.atlassian.jira.rest.client.api.GetCreateIssueMetadataOptionsBuilder;
 import com.atlassian.jira.rest.client.api.IssueRestClient;
 import com.atlassian.jira.rest.client.api.JiraRestClient;
+import com.atlassian.jira.rest.client.api.domain.CimProject;
 import com.atlassian.jira.rest.client.api.domain.Issue;
 import com.atlassian.jira.rest.client.api.domain.SearchResult;
 import com.atlassian.jira.rest.client.api.domain.TotalCount;
@@ -12,9 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static java.util.Arrays.stream;
@@ -27,15 +28,8 @@ public class SearchForIssuesUsingJQL {
     private JiraRestClient jiraRestClient;
     @Value("${jira.jql}")
     private String jiraQuery;
-    @Value("${export.columns.names}")
-    private String fields;
     @Value("${jira.changelogs}")
     private boolean includeChangeLogs;
-
-    public static Set<String> defaultFieldsToReturnInQuery() {
-        return Set.of("created", "project", "updated");
-    }
-
 
 
     public List<Issue> findAll() {
@@ -44,17 +38,13 @@ public class SearchForIssuesUsingJQL {
             Promise<TotalCount> totalCountPromise = jiraRestClient.getSearchClient().totalCount(jiraQuery);
             int total = totalCountPromise.claim().getCount();
 
-            Set<String> fieldsToReturnInQuery = stream(fields.split(","))
-                                                    .map(s->s.replaceAll(" ","").toLowerCase())
-                                                    .collect(toSet());
-            fieldsToReturnInQuery.addAll(defaultFieldsToReturnInQuery());
             logger.info("Total of {} entries found", total);
             String nextPageToken = null;
             while (result.size() < total) {
                 logger.info("Retrieving entries, percentage done: {}%", (result.size() * 100) / total);
                 Promise<SearchResult> searchResultPromise =
                         jiraRestClient.getSearchClient()
-                                .enhancedSearchJql(jiraQuery, null, nextPageToken, fieldsToReturnInQuery, null);
+                                .enhancedSearchJql(jiraQuery, null, nextPageToken, Set.of("*all"), null);
                 SearchResult searchResult = searchResultPromise.claim();
                 nextPageToken = searchResult.getNextPageToken();
                 searchResult.getIssues().forEach(result::add);
